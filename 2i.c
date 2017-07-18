bad1176d7923c51176f144923d3d1e117998c8bc
sriharikul <222srihari@gmail.com>
2017-07-18 17:31:52 +0530

    Add files via upload

---
+++ 2i.c
@@ -0,0 +1,11 @@
+/*finding area of retangle*/
+
+#include<stdio.h>
+int main()
+{
+	int l,b;
+	printf("Enter  value l and b of rectangle\n");
+	scanf("%d%d",&l,&b);
+	printf("area=%d\n",l*b);
+return 0;
+}
